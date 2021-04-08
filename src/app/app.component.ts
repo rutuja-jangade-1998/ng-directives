dbcda67d1ef932350174ce6196eaddc478b4aa81
rutuja <rjangade54@gmail.com>
2021-04-08 17:19:55 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngdirective';
+title = 'angularproject';
+fname = 'Rutuja';
+lname= 'Jangade';
+areacode = '+91'
+number= '8484911666'
+email = 'rutujajangade98@gmail.com'
+attendees= '30'
+position= 'Head of the Department';
+department= 'Marketing';
+contact = '1234567890'
+date= '05-04-2021'
+StartTime= '12pm';
+EndTime= '4pm';
+SplInst= 'It is mandatory for Everybody to sanitised himself/herself and must wear Mask in the conference room.';
 }
